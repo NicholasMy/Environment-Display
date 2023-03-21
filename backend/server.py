8426e63057873b85c317869a90807ac2c0c3ed17
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-03-21 10:57:24 -0400

    Configure CORS

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,9 @@
 from random import randint
-
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:*"}})
 
 @app.route("/")
 def api():
