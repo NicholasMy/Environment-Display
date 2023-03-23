e88924a7e90fffbd8275ead785c8472240ce7545
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2023-03-23 16:57:39 -0400

    Switch to Vuetify UI library

--- frontend/Environment-Display/src/App.vue
+++ frontend/Environment-Display/src/App.vue
@@ -10,35 +10,67 @@ const environmentDataStore = useEnvironmentDataStore()
 fetch('http://localhost:8085/rooms')
     .then(res => res.json())
     // .then(json => {environmentDataStore.rooms = json.rooms})
-    .then(json => {Object.assign(environmentDataStore.rooms, json.rooms)})
-    // That was the major bug! We can't directly reassign to a reactive, or we lose the reactivity.
+    .then(json => {
+      Object.assign(environmentDataStore.rooms, json.rooms)
+    })
+// That was the major bug! We can't directly reassign to a reactive, or we lose the reactivity.
 
 </script>
 
 <template>
-  <header>
-    <nav>
-      Environment Display
-      <ul>
-        <li>
-          <RouterLink :to="{name: 'home'}">Home</RouterLink>
-        </li>
 
-        <li v-for="room in environmentDataStore.rooms" :key="room.name">
-          <RouterLink :to="{name: 'room', params: {room: room.name}}">{{ room.friendly_name }}</RouterLink>
-        </li>
+  <v-app>
 
-      </ul>
-    </nav>
-  </header>
-  <main>
+    <v-app-bar color="#005bbb" flat>
 
-    <div v-if="!environmentDataStore.websocketConnected">
-      <h1>Connecting to WebSocket...</h1>
-      <p>If this takes too long, ensure the backend API is running.</p>
-    </div>
-    <RouterView v-else/>
-  </main>
+      <v-tabs hide-slider>
+        <v-tab :to="{name: 'home'}">
+            Environment Display
+        </v-tab>
+      </v-tabs>
+
+      <v-spacer/>
+
+      <v-tabs hide-slider>
+        <v-tab :to="{name: 'room', params: {room: room.name}}" v-for="room in environmentDataStore.rooms" :key="room.name">
+          {{ room.friendly_name }}
+        </v-tab>
+      </v-tabs>
+
+
+      <!--      <v-tabs>-->
+      <!--        <v-tab>-->
+      <!--          <RouterLink :to="{name: 'home'}">Home</RouterLink>-->
+      <!--        </v-tab>-->
+      <!--        <v-tab v-for="room in environmentDataStore.rooms" :key="room.name">-->
+      <!--          <RouterLink :to="{name: 'room', params: {room: room.name}}">{{ room.friendly_name }}</RouterLink>-->
+      <!--        </v-tab>-->
+
+
+      <!--      </v-tabs>-->
+
+
+      <!--                  <ul>-->
+      <!--                    <li>-->
+      <!--                      <RouterLink :to="{name: 'home'}">Home</RouterLink>-->
+      <!--                    </li>-->
+
+      <!--                    <li v-for="room in environmentDataStore.rooms" :key="room.name">-->
+      <!--                      <RouterLink :to="{name: 'room', params: {room: room.name}}">{{ room.friendly_name }}</RouterLink>-->
+      <!--                    </li>-->
+
+      <!--                  </ul>-->
+    </v-app-bar>
+    <v-main>
+
+      <div v-if="!environmentDataStore.websocketConnected">
+        <h1>Connecting to WebSocket...</h1>
+        <p>If this takes too long, ensure the backend API is running.</p>
+      </div>
+      <RouterView v-else/>
+    </v-main>
+
+  </v-app>
 
 </template>
 
--- frontend/Environment-Display/src/main.ts
+++ frontend/Environment-Display/src/main.ts
@@ -6,11 +6,16 @@ import router from './router'
 
 // import './assets/main.css'
 
-import './assets/mvp.css'
+// import './assets/mvp.css'
 import './assets/custom.css'
+
+import Vuetify from "@/plugins/vuetify";
+import 'vuetify/dist/vuetify.min.css'
+
 
 const app = createApp(App)
 
+app.use(Vuetify)
 app.use(createPinia())
 app.use(router)
 
